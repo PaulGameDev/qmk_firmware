cc68133f4c4aa8c73e7d78e4efad0147d152b630
PaulGameDev <paulskapczyk@gmail.com>
2021-04-03 00:53:32 +0200

    Add initial BMMP v1.0 support

---
+++ drivers/sonix/sled1734x.c
@@ -0,0 +1,14 @@
+#include "i2c_master.h"
+#include "sled1734x.h"
+
+void SLED1734X_write_registers(uint8_t reg, uint8_t data) {
+
+}
+
+void IS31FL3731_write_pwm_buffer(uint8_t addr, uint8_t *pwm_buffer) {
+
+}
+
+void SLED1734X_init(void) {
+
+}
